3ca1b9b8b97123bd60991d9a9e43a54584851b2c
William Mosquera <wjmmk3@gmail.com>
2022-11-28 11:27:53 -0500

    File App.ts Updated

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express  from 'express';
 import cors from 'cors'
 import { router } from './Routes';
-import dbMongo from './config/mongo';
+import dbMongo from './Config/mongo';
 
 const PORT = process.env.PORT || 3001;
 
